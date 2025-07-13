473767f750b1cf875660223069689b1867318bf9
AnasIqbaI <arafdarky@gmail.com>
2025-07-13 23:02:14 +0600

    Add files via upload

---
+++ Operators_Data_types.py
@@ -0,0 +1,23 @@
+name = "penguin"
+age = 5
+is_student = True
+weight = 3.5
+
+print("Name:", name)
+print("Type of name:", type(name))
+
+print("Age:", age)
+print("Type of age:", type(age))
+print("Is Student:", is_student)
+print("Type of is_student:", type(is_student))
+print("Weight:", weight)
+print("Type of weight:", type(weight))
+
+print("/n After typing Casting.....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
+
